cc9e4de3239abf6312d371df7b4d873fefc4e284
Vincent Kleis <vincent.s.kleis@gmail.com>
2024-04-18 11:42:22 +0200

    made an executable for windows

---
+++ sentimentanalyse_windows.py
@@ -0,0 +1,26 @@
+import pickle
+from sentimentanalyse.sentimentanalyse_funcs import lemmatizer
+from os import system, getcwd
+import nb_core_news_md
+
+nlp = nb_core_news_md.load()
+
+current = getcwd()
+
+pipe = pickle.load(open(f".\windows_executable\_internal\language_model.sav", 'rb'))
+sent = ''
+result = None
+
+# the loop
+while True:
+    system('cls')
+    if result != None:
+        print(f"the sentence is {result[0]}")
+
+    sent = [input("Type 'stop' to stop \nType a sentence in norwegian:")]
+    
+    if sent[0].lower() == 'stop':
+        break
+
+    sent = lemmatizer(sent, nlp, True)
+    result = pipe.predict(sent)
